d8db9ca25e08a2bcd3e873c5d3288cdef8d83126
theringsofsaturn <emiliankasemi@gmail.com>
2021-11-26 03:00:05 +0100

    topbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import Topbar from './topbar/TopBar';
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Switch, Route, Routes} from "react-router-dom";
+
 function App() {
-  return <div className="App">Blog App</div>;
+  return (
+    <Router>
+    <div className="App">
+      <Topbar />
+    </div>
+      </Router>
+  );
 }
 
 export default App;
---
+++ src/topbar/TopBar.jsx
@@ -0,0 +1,58 @@
+import "./topbar.css";
+import { Link } from "react-router-dom";
+
+const TopBar = () => {
+  return (
+    <div className="top">
+      <div className="topLeft">
+        <i className="topIcon fab fa-facebook-square"></i>
+        <i className="topIcon fab fa-instagram-square"></i>
+        <i className="topIcon fab fa-pinterest-square"></i>
+        <i className="topIcon fab fa-twitter-square"></i>
+      </div>
+      <div className="topCenter">
+        <ul className="topList">
+          <li className="topListItem">
+            <Link className="link" to="/">
+              HOME
+            </Link>
+          </li>
+          <li className="topListItem">ABOUT</li>
+          <li className="topListItem">CONTACT</li>
+          <li className="topListItem">
+            <Link className="link" to="/write">
+              WRITE
+            </Link>
+          </li>
+          {/* {user && <li className="topListItem">LOGOUT</li>} */}
+        </ul>
+      </div>
+      <div className="topRight">
+        (
+        <Link className="link" to="/settings">
+          <img
+            className="topImg"
+            src="https://images.gr-assets.com/users/1632342929p8/140785582.jpg"
+            alt=""
+          />
+        </Link>
+        ) : (
+        <ul className="topList">
+          <li className="topListItem">
+            <Link className="link" to="/login">
+              LOGIN
+            </Link>
+          </li>
+          <li className="topListItem">
+            <Link className="link" to="/register">
+              REGISTER
+            </Link>
+          </li>
+        </ul>
+        <i className="topSearchIcon fas fa-search"></i>
+      </div>
+    </div>
+  );
+};
+
+export default TopBar;
