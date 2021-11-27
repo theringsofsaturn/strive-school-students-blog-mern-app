de6aa99d797d7418811975ed53f4c90f5ce2bc94
theringsofsaturn <emiliankasemi@gmail.com>
2021-11-27 02:07:51 +0100

    added routers

--- src/App.js
+++ src/App.js
@@ -16,17 +16,32 @@ import Settings from "./pages/settings/Settings";
 import Login from "./pages/Login/Login";
 import Register from "./pages/register/Register";
 
-
 function App() {
+  const currentUser = true;
   return (
     <Router>
       <Topbar />
-      <Register />
-      {/* <Login/> */}
-      {/* <Settings/> */}
-      {/* <Write/> */}
-      {/* <Single /> */}
-      {/* <Homepage /> */}
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+
+        <Route path="/posts" element={<Homepage />} />
+
+        <Route
+          path="/register"
+          element={currentUser ? <Homepage /> : <Register />}
+        />
+
+        <Route path="/login" element={currentUser ? <Homepage /> : <Login />} />
+
+        <Route path="/post/:id" element={<Single />} />
+
+        <Route path="/write" element={currentUser ? <Write /> : <Login />} />
+
+        <Route
+          path="/settings"
+          element={currentUser ? <Settings /> : <Login />}
+        />
+      </Routes>
     </Router>
   );
 }
