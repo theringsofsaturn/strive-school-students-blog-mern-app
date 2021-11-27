25eeb9b8d9102886f9fc023a3f1d388185e91211
theringsofsaturn <emiliankasemi@gmail.com>
2021-11-27 20:37:43 +0100

    user schema added

---
+++ api/models/User.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose"
+
+const UserSchema = new mongoose.Schema(
+    {
+      username: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+      password: {
+        type: String,
+        required: true,
+      },
+      profilePic: {
+        type: String,
+        default: "",
+      },
+    },
+    { timestamps: true }
+  );
+  
+  module.exports = mongoose.model("User", UserSchema);
