a47f9935391e5bbd88f8f6be53e7a0ffd8903e94
theringsofsaturn <emiliankasemi@gmail.com>
2021-11-27 01:45:24 +0100

    fixes

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import Settings from "./pages/settings/Settings";
 import Login from "./pages/Login/Login";
 import Register from "./pages/register/Register";
 
+
 function App() {
   return (
     <Router>
