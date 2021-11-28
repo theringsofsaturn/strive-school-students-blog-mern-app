a1410169ae6b37e46b169b7b1516640d6661fec4
theringsofsaturn <emiliankasemi@gmail.com>
2021-11-28 16:11:20 +0100

    Fetching posts from API

--- client/src/pages/homepage/Homepage.jsx
+++ client/src/pages/homepage/Homepage.jsx
@@ -3,13 +3,28 @@ import Header from "../../component/header/Header";
 import Posts from "../../component/posts/Posts";
 import Sidebar from "../../component/sidebar/Sidebar";
 import "./homepage.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Homepage = () => {
+  const [posts, setPosts] = useState([]);
+  const { search } = useLocation();
+
+  useEffect(() => {
+    // When we want to use axios, we need a variable to store the response, as we are using async/await
+    const fetchPosts = async () => {
+      const res = await axios.get("http://localhost:3001/api/posts" + search);
+      setPosts(res.data);
+      console.log("Posts Data *****",res.data);
+    };
+    fetchPosts();
+  }, [search]);
+
   return (
     <>
       <Header />
       <div className="home">
-        <Posts />
+        <Posts posts={posts} />
         <Sidebar />
       </div>
     </>
