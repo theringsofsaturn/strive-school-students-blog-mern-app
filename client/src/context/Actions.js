68df565c68d72862c8adc3c13549f1779492d2a9
theringsofsaturn <emiliankasemi@gmail.com>
2021-11-29 19:52:12 +0100

    Created context with initial state

---
+++ client/src/context/Actions.js
@@ -0,0 +1,11 @@
+import { createContext, useEffect, useReducer } from "react";
+import Reducer from "./Reducer";
+
+const INITIAL_STATE = {
+  user: JSON.parse(localStorage.getItem("user")) || null,
+  isFetching: false,
+  error: false,
+};
+
+
+export const Context = createContext(INITIAL_STATE);
