e6abb915ddb29b7acb4da211a757d8acd96b2006
theringsofsaturn <emiliankasemi@gmail.com>
2021-11-30 14:26:35 +0100

    Delete post added
    Working correctly

--- client/src/component/singlePost/SinglePost.jsx
+++ client/src/component/singlePost/SinglePost.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import { useLocation } from "react-router";
 import axios from "axios";
 import { useContext, useEffect, useState } from "react";
+import { Context } from "../../context/Context";
 import "./singlePost.css";
 
 const SinglePost = () => {
@@ -10,9 +11,10 @@ const SinglePost = () => {
   const path = location.pathname.split("/")[2]; // Split the pathname by "/" and get the third element of the array -> [2] index. This is the post id. -> e.g. "/post/123456" -> ["before /", "post", "123456"] -> "123456"
 
   // **************** STATES ********************
-  const [post, setPost] = useState({}); 
-  const [title, setTitle] = useState(""); 
-  const [desc, setDesc] = useState(""); 
+  const [post, setPost] = useState({});
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const { user } = useContext(Context);
   const publicFolder = "http://localhost:3001/images/";
 
   useEffect(() => {
@@ -26,19 +28,39 @@ const SinglePost = () => {
     getPost();
   }, [path]); // When the path changes, we want to fetch again.
 
+  // DELETE the post on click
+  const handleDelete = async () => {
+    try {
+      await axios.delete(`http://localhost:3001/api/posts/${post._id}`, {
+        data: { username: user.username }, // We can send this data directly because we are using Delete method, not Post. We can write data here. It will not work as with Post method above: + path, { username: user.username }
+      });
+      window.location.replace("/"); // After deleting the post, we want to go back to the home page.
+    } catch (err) {}
+  };
+
   return (
     <div className="singlePost">
       <div className="singlePostWrapper">
         {/* If there's post image, show it, if not, just don't show any image */}
         {post.photo && (
-          <img src={publicFolder + post.photo} alt="" className="singlePostImg" />
+          <img
+            src={publicFolder + post.photo}
+            alt=""
+            className="singlePostImg"
+          />
         )}
         <h1 className="singlePostTitle">
           {post.title}
-          <div className="singlePostEdit">
-            <i className="singlePostIcon far fa-edit"></i>
-            <i className="singlePostIcon far fa-trash-alt"></i>
-          </div>
+          {/* If the post's username is the same as the logged in user, then we can edit or delete the post. Also if there's no user (user?) it will not check for any username after, so we will not have any error */}
+          {post.username === user?.username && (
+            <div className="singlePostEdit">
+              <i className="singlePostIcon far fa-edit"></i>
+              <i
+                className="singlePostIcon far fa-trash-alt"
+                onClick={handleDelete}
+              ></i>
+            </div>
+          )}
         </h1>
         <div className="singlePostInfo">
           <span className="singlePostAuthor">
