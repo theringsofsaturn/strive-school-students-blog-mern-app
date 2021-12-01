33001e6fe016cf2e0990d76d814350eac8061c79
theringsofsaturn <emiliankasemi@gmail.com>
2021-12-01 04:38:44 +0100

    changes in design

--- client/src/component/header/Header.jsx
+++ client/src/component/header/Header.jsx
@@ -1,19 +1,23 @@
 import "./header.css";
-
+import titleLogo from "../../assets/blog1.png"
 const Header = () => {
   return (
     <div className="header">
       <div className="headerTitles">
-        <span className="headerTitleSm">Strive School Students</span>
-        <span className="headerTitleLg">BLOG</span>
+        <img  
+        className="headerImgTitle"
+        src={titleLogo} 
+        alt="" />
+        {/* <span className="headerTitleSm">Strive School Students</span> */}
+        {/* <span className="headerTitleLg">BLOG</span> */}
       </div>
-      <div>
+      {/* <div>
       <img
         className="headerImg"
         src="https://strive.school/assets/generics/covers/fullstack.png"
         alt=""
       />
-      </div>
+      </div> */}
       
     </div>
   );
